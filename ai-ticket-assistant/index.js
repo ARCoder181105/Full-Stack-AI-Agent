899e98927a4a68e07b6452f319b02f984b66e33b
Aditya Rana <adityarana181105@gmail.com>
2025-07-23 03:00:22 +0530

    done with backend

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -2,6 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import userRoutes from './routes/user.js'
+import ticketRoutes from './routes/ticket.js'
+import { serve } from 'inngest/express'
+import { inngest } from './inngest/client.js'
+import { onUserSignup } from './inngest/functions/on-signup.js'
+import { onTicketCreated } from './inngest/functions/on-ticket-create.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -11,7 +16,13 @@ const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json())
 
-app.use('/api/auth',userRoutes)
+app.use('/api/auth', userRoutes)
+app.use('/api/tickets', ticketRoutes)
+
+app.use('/api/inngest', serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated]
+}))
 
 
 mongoose
--- ai-ticket-assistant/routes/ticket.js
+++ ai-ticket-assistant/routes/ticket.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import { authenticate } from '../middlewares/auth.js'
+import { createTicket, getTickets, getTicket } from '../controllers/ticket.js'
+
 
 const router = express.Router();//best for writing the route in the different files and also for the mainteing large number of routes
 
+router.get('/', authenticate, getTickets)
+router.get('/:id', authenticate, getTicket)
+router.get('/', authenticate, createTicket)
 
 export default router;
